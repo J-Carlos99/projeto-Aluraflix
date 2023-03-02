31a6228db4f12e9e6fea92b3ee94078209892f42
J-Carlos99 <87335255+J-Carlos99@users.noreply.github.com>
2023-03-01 22:53:30 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+var listaFilmes = ["https://www.negocioefranquia.com/wp-content/uploads/2018/09/school-of-rock.jpg",
+                   "https://1.bp.blogspot.com/-ImZPRqLsluE/WFK156_6pNI/AAAAAAAAYBY/0lEhNRF5wfQdLfr6hpT57_Jt2eBrE9H5wCLcB/s1600/arrival-kartoun-desert.jpg",
+                   
+                   "https://img.elo7.com.br/product/zoom/236462D/big-poster-filme-homem-aranha-no-aranhaverso-90x60-cm-lo-003-quadro.jpg",
+                   "https://www.minhavidaliteraria.com.br/wp-content/uploads/2013/10/23233.jpg",
+                   "https://http2.mlstatic.com/D_NQ_NP_14658-MLB4328088840_052013-O.webp"]
+
+for(var i = 0; i < listaFilmes.length; i++){
+  document.write("<img src=" + listaFilmes[i] + ">");
+}
+
+
